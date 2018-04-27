4cc3ee33f888a3caa2d3ba9d04de2f8fecfc0989
lucasmsp <lucasmsp@gmail.com>
2018-04-27 14:50:10 -0300

    adding new version of tests

--- Python/API/test/app.py
+++ Python/API/test/app.py
@@ -6,49 +6,47 @@ __email__ = "lucasmsp@gmail.com"
 from pycompss.api.task import task
 from pycompss.api.parameter import *
 from pycompss.api.api import compss_wait_on
-import hdfs_pycompss.hdfsConnector as hdfs
+from hdfspycompss.HDFS import HDFS
+from hdfspycompss.Block import Block
 import time
 
-
-def getSuffixes(length):
-    import itertools
-    import math
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    r = int(math.ceil(float(length)/len(alpha)))
-    product = list(itertools.product(alpha, repeat=r))[:length]
-    return sorted(["".join(x) for x in product])
 
 def test01_mkdir_and_write():
     """Test: create a new folder and write splitted data."""
     numFrag = 4
-    hdfs_ops = dict()
-    hdfs_ops['host'] = 'default'
-    hdfs_ops['port'] = 0
-    hdfs_ops['path'] = '/test_Integration'
-    hdfs.mkdir(hdfs_ops)
+    path = '/test-hdfspycompss'
+    dfs = HDFS(host='localhost', port=9000)
+    dfs.mkdir(path)
 
-    suffixes = getSuffixes(numFrag)
-    hdfs_ops['path'] = '/test_Integration/file'
+    suffixes = dfs.get_suffixes(numFrag)
+    filename = '/test-hdfspycompss/file'
 
     start = time.time()
-    info = [writeData(suff, hdfs_ops) for suff in suffixes]
+    info = [writeData(suff, filename) for suff in suffixes]
     info = compss_wait_on(info)
     print info
     end = time.time()
     print "Time to write: {}".format(end-start)
 
 
-def test02_merge():
+@task(returns=int)
+def writeData(id_block, filename):
+    """Create a DataFrame."""
+    filename = '{}_{}'.format(filename, id_block)
+    data = "Lorem ipsum dolor sit amet, "\
+           "consectetur adipiscing elit.\nIn a mi eget libero"\
+           "volutpat efficitur.\n"
+    return HDFS(host='localhost', port=9000).writeBlock(filename, data)
 
-    hdfs_ops = dict()
-    hdfs_ops['host'] = 'default'
-    hdfs_ops['port'] = 0
-    hdfs_ops['path'] = '/test_Integration/file\*'
-    hdfs_ops['dst'] = '/test_Integration/test02_merged'
-    hdfs_ops['wait'] = True
-    hdfs_ops['rm'] = False  # True
+
+def test02_merge():
+    """Merge multiple files in a specific folder in one file."""
+    src = '/test-hdfspycompss/file\*'
+    dst = '/test-hdfspycompss/test02_merged'
+    wait = True
+    rm = False
     start = time.time()
-    info = hdfs.mergeFiles(hdfs_ops)
+    info = HDFS().mergeFiles(src, dst, wait, rm)
     info = compss_wait_on(info)
     print info
 
@@ -58,14 +56,11 @@ def test02_merge():
 
 def test03_read():
     """Test: read a file in HDFS."""
-    numFrag = 4
-    hdfs_ops = dict()
-    hdfs_ops['host'] = 'default'
-    hdfs_ops['port'] = 0
-    hdfs_ops['path'] = '/test_Integration/test02_merged'
+    numFrag = 3
+    path = '/test-hdfspycompss/test02_merged'
     start = time.time()
 
-    HDFS_BLOCKS = hdfs.findNBlocks(hdfs_ops, numFrag)
+    HDFS_BLOCKS = HDFS().findNBlocks(path, numFrag)
     print "[INFO] - Number of Blocks: {}".format(len(HDFS_BLOCKS))
     for b in HDFS_BLOCKS:
         print "[INFO] - Block: {}".format(b)
@@ -80,28 +75,13 @@ def test03_read():
         for line in frag:
             print (">"+line),
 
-    # or:
-    # for frag in data:
-    #    print frag.getvalue()
-
     print "Time to read: {}".format(end-start)
 
 
 @task(returns=list)
 def readData(block):
-    return hdfs.readBlock(block)
-
-
-@task(returns=int)
-def writeData(id_block, txt_options):
-    """Create a DataFrame."""
-    txt_options['path'] = '{}_{}'.format(txt_options['path'], id_block)
-    msg = "Lorem ipsum dolor sit amet, "\
-          "consectetur adipiscing elit.\nIn a mi eget libero"\
-          "volutpat efficitur.\n"
-
-    return hdfs.writeBlock(txt_options, msg)
-
+    """Read fragment."""
+    return Block(block).readBlock()
 
 
 if __name__ == "__main__":
